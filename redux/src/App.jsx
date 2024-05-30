15737498b656ce2379d79fdc8a4ac3d151619771
1anshu-stack <71838931+1anshu-stack@users.noreply.github.com>
2024-05-30 08:03:11 +0000

    updateremain

---
+++ redux/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import AddTodo from './components/addtodo'
+import Todos from './components/todo'
+
+function App() {
+
+  return (
+    <>
+      <h1>Using Redux Toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
