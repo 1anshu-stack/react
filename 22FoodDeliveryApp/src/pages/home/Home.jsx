a206ea1d186859c9f2e2e3c6e28065745e69ecc2
1anshu-stack <71838931+1anshu-stack@users.noreply.github.com>
2024-07-23 17:39:04 +0000

    components

--- 22FoodDeliveryApp/src/pages/home/Home.jsx
+++ 22FoodDeliveryApp/src/pages/home/Home.jsx
@@ -1,10 +1,14 @@
 import "./home.css"
 import Header from "../../components/header/Header";
+import ExploreMenu from "../../components/ExploreMenu/ExploreMenu";
+import { useState } from "react";
 
 const Home = () => {
+    const [category, setCategory] = useState("All")
     return (
         <>
            <Header />
+           <ExploreMenu category={category} setCategory={setCategory} />
         </>
     )
 }
